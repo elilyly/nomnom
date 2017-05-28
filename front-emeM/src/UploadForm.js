514c879ff9dd159c604163b178e8838814c3faf0
hellosylvee <helloimsylvia@gmail.com>
2017-05-28 14:43:10 -0400

    Adding in upload form component and dependencies

--- front-emeM/src/UploadForm.js
+++ front-emeM/src/UploadForm.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
+import request from 'superagent'
 
-class UploadForm extends React.Component {
-  constructor() {
-    super()
+const CLOUDINARY_UPLOAD_PRESET = 'dmtif1gy';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/hellosylvee/image/upload'
 
+class UploadForm extends Component {
+  constructor(props) {
+    super(props)
 
     this.state = {
-      uploadInput: '',
-      description: ''
+      uploadedFileCloudinaryUrl: ''
     }
   }
 
-  handleInputChange(event) {
+  onImageDrop(files) {
     this.setState({
-      uploadInput: event.target.value
+      uploadedFile: files[0]
     })
+
+    this.handleImageUpload(files[0])
   }
 
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                        .field('file', file);
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        })
+      }
+    })
+  }
 
   render() {
     return(
       <div className="ui page grid main">
         <div  className="row">
-          <form>
-            <h1>Upload Image</h1>
-            <input type='text' value={this.state.uploadInput}/>
-          </form>
+          <Dropzone multiple={false} accept="image/*" onDrop={this.onImageDrop.bind(this)}>
+            <p>Drop an image or click to select a file to upload.</p>
+          </Dropzone>
         </div>
       </div>
     )
---
+++ front-emeM/src/UploadFormContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class UploadFormContainer extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default UploadFormContainer
