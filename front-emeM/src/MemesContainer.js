1882f4e2a7fbe41fa0cad66be485d5e8fe741158
github username <github email address>
2017-05-30 11:37:47 -0400

    create MemesContainer to hold our app's state

---
+++ front-emeM/src/MemesContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import UploadForm from './UploadForm'
+
+class MemesContainer extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      memes: []
+    }
+  }
+
+  handleAddMeme(meme){
+    this.setState( prevState => ({ memes: [...prevState.memes, meme] }) )
+  }
+
+  render(){
+    console.log("MemesContainer state", this.state)
+    return(
+      <UploadForm onSubmit={this.handleAddMeme.bind(this)} />
+    )
+  }
+
+}
+
+export default MemesContainer
