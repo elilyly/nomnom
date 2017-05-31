19975bd0dfbe3f0baecbdd9f4da5c570bf03a27e
Elizabeth <elizabethnich19@gmail.com>
2017-05-31 16:11:23 -0400

    Revert "modal feature"

--- front-emeM/src/DisplayAllMemesInRow.js
+++ front-emeM/src/DisplayAllMemesInRow.js
@@ -1,30 +1,20 @@
 import React from 'react'
-import { Modal, Image } from 'semantic-ui-react'
 
 const DisplayAllMemesInRow = (props) => {
   const reverseOrder = props.memes.slice(0).reverse()
   let eachMeme = reverseOrder.map( (em, i) => {
     return(
       <div className="wrapper" key={i}>
-        <Modal trigger={<div><img className="image-tile" src={em.image_url} alt="" />
+        <img className="image-tile" src={em.image_url} alt="" />
         <div className="display-text-center">
           <div id="display-text-top">{em.text_top.toUpperCase()}</div>
           <div id="display-text-bottom">{em.text_bottom.toUpperCase()}</div>
-        </div></div>}>
-
-          <div><Modal.Content>
-            <div><img className="image-tile" src={em.image_url} alt="" />
-            <div className="display-text-center">
-              <div id="display-text-top">{em.text_top.toUpperCase()}</div>
-              <div id="display-text-bottom">{em.text_bottom.toUpperCase()}</div>
-            </div></div>
-          </Modal.Content></div>
-        </Modal>
+        </div>
       </div>
     )
   })
   return(
-    <div> {eachMeme} </div>
+    <div>{ eachMeme }</div>
   )
 }
 
