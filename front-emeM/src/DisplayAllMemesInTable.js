fdcd909c93492159258dc048b505a72ca2204aa2
hellosylvee <helloimsylvia@gmail.com>
2017-05-31 09:31:03 -0400

    added font-family

--- front-emeM/src/DisplayAllMemesInTable.js
+++ front-emeM/src/DisplayAllMemesInTable.js
@@ -2,10 +2,6 @@ import React from 'react'
 import DisplayAllMemesInRow from './DisplayAllMemesInRow'
 
 const DisplayAllMemesinTable = (props) => {
-  // let row = []
-  // for (var i = 0; i < props.memes.length; i++) {
-  //   if(i % 4 === 0) { console.log(row.push(props.memes)) }
-  // }
 
   return(
     <div>
